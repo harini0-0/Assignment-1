f92a5b943aba91a301613812c0de5802d655e8fa
harini0-0 <harinipri2001@gmail.com>
2026-05-16 00:40:19 -0400

    added Javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const header = document.querySelector("main h1");
+
+    header.addEventListener("click", () => {
+
+        alert("Welcome to the Vanilla HTML + CSS + JS Assignment!");
+
+    });
+
+});
